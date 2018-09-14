8fff46099d5abcb95bf1f2c8deb5e13794d7b78e
kent <kent.dang1706@gmail.com>
2018-09-14 16:35:38 +0700

    Add code

---
+++ application/logs/log-2018-09-14.php
@@ -0,0 +1,63 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-09-14 04:26:41 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:26:44 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:26:51 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:26:51 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:26:51 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:26:54 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:15 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:17 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:19 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:26 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:27 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:27 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:28 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:30 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:37 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:40 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:42 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:51 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:52 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:27:53 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:28:00 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:29:35 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/application/controllers/Loader.php 64
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/application/controllers/Loader.php 64
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/application/controllers/Loader.php 47
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/application/controllers/Loader.php 27
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/application/controllers/Loader.php 81
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/application/controllers/Loader.php 81
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/core/Common.php 570
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:34:12 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/application/controllers/Loader.php 112
+ERROR - 2018-09-14 04:43:17 --> Severity: Warning --> session_start(): Cannot send session cache limiter - headers already sent (output started at /Volumes/WORK-SOFT/ROOT/phusi1/index.php:38) /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 143
+ERROR - 2018-09-14 04:43:17 --> Severity: Warning --> session_regenerate_id(): Cannot regenerate session id - headers already sent /Volumes/WORK-SOFT/ROOT/phusi1/system/libraries/Session/Session.php 718
+ERROR - 2018-09-14 04:46:35 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:47:19 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:47:19 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:47:20 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:48:49 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:48:49 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 04:48:49 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:18:09 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:06 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:24 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:26 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:30 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:30 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:30 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:30 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:30 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:30 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:19:31 --> 404 Page Not Found: /index
+ERROR - 2018-09-14 06:29:45 --> 404 Page Not Found: /index
