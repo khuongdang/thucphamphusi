b05c72eb1efa505a3d3843c21876dded08d82f1a
kent <kent.dang1706@gmail.com>
2018-09-17 17:04:31 +0700

    update code

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'http://phusi1.local/';
+$config['base_url'] = 'http://thucphamphusi.local/';
 
 /*
   |--------------------------------------------------------------------------
